63fd0b8eea257163921e285f00d58c6b5fed0f49
David Spera <dspera@gmail.com>
2026-08-01 14:15:14 -0400

    feat: add validation middleware for book ID in routes

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const controller = require('../controllers/books.controller');
 const validateBook = require('../middleware/validateBook');
+const validateId = require('../middleware/validateId');
 
 const router = Router();
 
 router.get('/', controller.listBooks);
-router.get('/:id', controller.getBook);
+router.get('/:id', validateId, controller.getBook);
 router.post('/', validateBook, controller.createBook);
-router.put('/:id', validateBook, controller.updateBook);
-router.delete('/:id', controller.deleteBook);
+router.put('/:id', validateId, validateBook, controller.updateBook);
+router.delete('/:id', validateId, controller.deleteBook);
 
 module.exports = router;
